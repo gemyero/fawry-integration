79c7b2e9e0ad4ab4c7b5579ad4cc263fa067f9a1
Mohamed Gamal <mohamed.rady@fixedmea.com>
2019-07-31 13:18:17 +0200

    add run function to renew url

--- index.js
+++ index.js
@@ -8,89 +8,93 @@ const computeHashSHA256 = data => crypto
   .update(data)
   .digest('hex');
 
-const language = 'ar-eg';
-const merchantCode = '1tSa6uxz2nSL2tCkXCgN7A==';
-const merchantRefNumber = uniqid.time();
-const customerProfileId = uniqid.time();
-const successPageUrl = 'http://localhost:4000/successUrl';
-const failerPageUrl = 'http://localhost:4000/failerUrl';
-const items = [
-  { productSKU: uniqid.time(), description: 'product 1', price: '50', quantity: '2' },
-  { productSKU: uniqid.time(), description: 'product 2', price: '70', quantity: '1' }
-];
+function run() {
+  const language = 'ar-eg';
+  const merchantCode = '1tSa6uxz2nSL2tCkXCgN7A==';
+  const merchantRefNumber = uniqid.time();
+  const customerProfileId = uniqid.time();
+  const successPageUrl = 'http://localhost:4000/successUrl';
+  const failerPageUrl = 'http://localhost:4000/failerUrl';
+  const items = [
+    { productSKU: uniqid.time(), description: 'product 1', price: '50', quantity: '2' },
+    { productSKU: uniqid.time(), description: 'product 2', price: '70', quantity: '1' }
+  ];
 
-const data = prepareDataForHashing({
-  merchantCode,
-  merchantRefNumber,
-  customerProfileId,
-  items,
-  expiry: '2',
-  mobile: '01091492532',
-  email: 'mohamedgr91@gmail.com',
-  secureHashKey: '25c89e69999d4cb8b6ba8b43dec3ea25',
-});
+  const data = prepareDataForHashing({
+    merchantCode,
+    merchantRefNumber,
+    customerProfileId,
+    items,
+    expiry: '2',
+    mobile: '01091492532',
+    email: 'mohamedgr91@gmail.com',
+    secureHashKey: '25c89e69999d4cb8b6ba8b43dec3ea25',
+  });
 
-const hash = computeHashSHA256(data);
+  const hash = computeHashSHA256(data);
 
-const prodBaseUrl = 'https://www.atfawry.com/ECommercePlugin/FawryPay.jsp';
-const sandboxBaseUrl = 'https://atfawry.fawrystaging.com/ECommercePlugin/FawryPay.jsp';
+  const prodBaseUrl = 'https://www.atfawry.com/ECommercePlugin/FawryPay.jsp';
+  const sandboxBaseUrl = 'https://atfawry.fawrystaging.com/ECommercePlugin/FawryPay.jsp';
 
-const url = `${sandboxBaseUrl}?chargeRequest={"language":"ar-eg","merchantCode":"${merchantCode}","merchantRefNumber":"${merchantRefNumber}","customer":{"name":"Mohamed Gamal","mobile":"01091492532","email":"mohamedgr91@gmail.com", "customerProfileId":"${customerProfileId}"},"order":{"description":"test bill inq", "expiry": "2", "orderItems":${JSON.stringify(items)}},"signature":"${hash}"}&successPageUrl=http://localhost:4000/successUrl&failerPageUrl=http://localhost:4000/failerUrl`;
+  const url = `${sandboxBaseUrl}?chargeRequest={"language":"ar-eg","merchantCode":"${merchantCode}","merchantRefNumber":"${merchantRefNumber}","customer":{"name":"Mohamed Gamal","mobile":"01091492532","email":"mohamedgr91@gmail.com", "customerProfileId":"${customerProfileId}"},"order":{"description":"test bill inq", "expiry": "2", "orderItems":${JSON.stringify(items)}},"signature":"${hash}"}&successPageUrl=http://localhost:4000/successUrl&failerPageUrl=http://localhost:4000/failerUrl`;
 
-console.log(`
-merchantCode: 1tSa6uxz2nSL2tCkXCgN7A==
-merchantRefNumber: ${merchantRefNumber}
-customerProfileId: ${customerProfileId}
-Item code:  3124124 
-Quantity: 2
-Price: 50.00
-Expiry: 2
-Mobile: 01091492532
-Email: mohamedgr91@gmail.com
-Secure hash key:  25c89e69999d4cb8b6ba8b43dec3ea25
-****************************************************
+  return url;
+  //   console.log(`
+  // merchantCode: 1tSa6uxz2nSL2tCkXCgN7A==
+  // merchantRefNumber: ${merchantRefNumber}
+  // customerProfileId: ${customerProfileId}
+  // Item code:  3124124 
+  // Quantity: 2
+  // Price: 50.00
+  // Expiry: 2
+  // Mobile: 01091492532
+  // Email: mohamedgr91@gmail.com
+  // Secure hash key:  25c89e69999d4cb8b6ba8b43dec3ea25
+  // ****************************************************
 
-resulting data before hash:
-***************************
-${data}
+  // resulting data before hash:
+  // ***************************
+  // ${data}
 
-resulting hash:
-**************
-${hash}
+  // resulting hash:
+  // **************
+  // ${hash}
 
-resulting request:
-******************
-${url}
+  // resulting request:
+  // ******************
+  // ${url}
 
-ERROR:
-******
-9903 : حدث خطا
+  // ERROR:
+  // ******
+  // 9903 : حدث خطا
 
-`);
+  // `);
+  }
 
-const app = express();
+  const app = express();
 
-app.use(express.static('public'));
+  app.use(express.static('public'));
 
-app.get('/payLink', (req, res) => {
-  res.redirect(url);
-});
+  app.get('/payLink', (req, res) => {
+    const url = run();
+    res.redirect(url);
+  });
 
-app.get('/successUrl', (req, res) => {
-  res.send({ chargeResponse: JSON.parse(req.query.chargeResponse) });
-});
+  app.get('/successUrl', (req, res) => {
+    res.send({ chargeResponse: JSON.parse(req.query.chargeResponse) });
+  });
 
-app.get('/failerUrl', (req, res) => {
-  res.send(req.query);
-});
+  app.get('/failerUrl', (req, res) => {
+    res.send(req.query);
+  });
 
-app.get('/serverCallbackV2', (req, res) => {
-  res.send('in get callback');
-});
+  app.get('/serverCallbackV2', (req, res) => {
+    res.send('in get callback');
+  });
 
-app.post('/serverCallbackV2', (req, res) => {
-  res.send('in post callback');
-});
+  app.post('/serverCallbackV2', (req, res) => {
+    res.send('in post callback');
+  });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log('server starts...'));
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log('server starts...'));
